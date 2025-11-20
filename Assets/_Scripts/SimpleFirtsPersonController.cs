4878cedf413d9cb511bf155d5530b497a566c3c7
tommasofracassi06 <tommaso.fracassi@itsrizzoli.it>
2025-11-20 16:39:56 +0100

    Player e addobbo scena

--- Assets/_Scripts/SimpleFirtsPersonController.cs
+++ Assets/_Scripts/SimpleFirtsPersonController.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class Mover : MonoBehaviour
+public class SimpleFirstPersonControllerr : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
